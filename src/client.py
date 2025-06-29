9acc6e7489a0ff08658755d94cf7fa6c009cf9ff
VitorCaversan <joaopassos@alunos.utfpr.edu.br>
2025-06-29 20:35:04 -0300

    Add replicaIgnore test.

--- src/client.py
+++ src/client.py
@@ -11,27 +11,32 @@ def run():
             continue
 
         cmd = cmd_inputs[0]
-        with grpc.insecure_channel(f'localhost:{gv.LEADER_PORT}') as channel:
-            stub = replic_pb2_grpc.ClientServiceStub(channel)
-            if cmd == "write" and len(cmd_inputs) >= 3:
-                response = stub.Query(replic_pb2.QueryRequest(key=cmd_inputs[1]))
-                if response.found:
-                    print("Key already exists. Use 'query' to check value.")
-                else:
-                    print(cmd_inputs)
-                    if len(cmd_inputs) == 4:
-                        response = stub.Write(replic_pb2.WriteRequest(key=cmd_inputs[1], value=cmd_inputs[2], testError=cmd_inputs[3]))
+        try:
+            with grpc.insecure_channel(f'localhost:{gv.LEADER_PORT}') as channel:
+                stub = replic_pb2_grpc.ClientServiceStub(channel)
+                if cmd == "write" and len(cmd_inputs) >= 3:
+                    
+                    response = stub.Query(replic_pb2.QueryRequest(key=cmd_inputs[1]))
+                    if response.found:
+                        print("Key already exists. Use 'query' to check value.")
                     else:
-                        response = stub.Write(replic_pb2.WriteRequest(key=cmd_inputs[1], value=cmd_inputs[2], testError="0"))
-                    print(f"Leader response: {response.message}")
-            elif cmd == "query" and len(cmd_inputs) == 2:
-                response = stub.Query(replic_pb2.QueryRequest(key=cmd_inputs[1]))
-                if response.found:
-                    print(f"Value: {response.value}")
+                        print(cmd_inputs)
+                        if len(cmd_inputs) == 4:
+                            response = stub.Write(replic_pb2.WriteRequest(key=cmd_inputs[1], value=cmd_inputs[2], testError=cmd_inputs[3]))
+                        else:
+                            response = stub.Write(replic_pb2.WriteRequest(key=cmd_inputs[1], value=cmd_inputs[2], testError="0"))
+                        print(f"Leader response: {response.message}")
+                elif cmd == "query" and len(cmd_inputs) == 2:
+                    response = stub.Query(replic_pb2.QueryRequest(key=cmd_inputs[1]))
+                    if response.found:
+                        print(f"Value: {response.value}")
+                    else:
+                        print("Key not found.")
                 else:
-                    print("Key not found.")
-            else:
-                print("Invalid command.")
+                    print("Invalid command.")
+        except grpc.RpcError as e:
+            print(f"gRPC error: {e.code()} - {e.details()}")
+            pass
 
 if __name__ == '__main__':
     run()
